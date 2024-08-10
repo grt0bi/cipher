0fc7339940fc893c238b2ec3e7dc5347bd5a8858
Tobias <113802267+grt0bi@users.noreply.github.com>
2024-08-10 11:41:32 +0200

    added comments to Engine logic

--- src/Cipher.java
+++ src/Cipher.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Cipher {
-    static boolean player = false;
+    static boolean player = true;
     static boolean boardFlipped = false;
 
     public static void main(String[] args) throws IOException {
@@ -17,14 +17,13 @@ public class Cipher {
         } else {
             System.out.println("Failed to load opening book. Proceeding without it.");
         }*/
-        Engine.engineMove(8, board);
+        Engine.engineMove(6, board);
         play(board, gui, 3000, player);
 
         Client client = new Client(1409);
 
         client.initMatch();
     }
-
 
     static void play(Board board, Gui gui, int timeLimit, boolean player) {
         while (MoveGeneration.getMoves(board).count > 0 && Repetition.getRepetitionAmount(board.zobristKey, Repetition.historyFlag) < 3) {
@@ -57,46 +56,8 @@ public class Cipher {
             }
         }
     }
-
-    static void engineTest(Board board, Gui gui, int timeLimit) {
-        //Scanner sc = new Scanner(System.in);
-        while (MoveGeneration.getMoves(board).count > 0 && Repetition.getRepetitionAmount(board.zobristKey, Repetition.historyFlag) < 3) {
-            System.out.println(board.boardToFen());
-            board = Engine.engineMove(board, timeLimit);
-            Repetition.addToHistory(board.zobristKey, Repetition.historyFlag);
-
-            gui.panel.board = board;
-            gui.repaint();
-            System.out.println();
-            //sc.nextInt();
-        }
-        if (board.player) {
-            System.out.printf("%.2f\n", Evaluation.evaluation(board)/100);
-        } else {
-            System.out.printf("%.2f\n", -Evaluation.evaluation(board)/100);
-        }
-        if (Repetition.getRepetitionAmount(board.zobristKey, Repetition.historyFlag) >= 3) {
-            System.out.println("Draw by repetition!");
-        } else {
-            if ((board.fKing & board.eAttackMask) != 0) {
-                System.out.println("Checkmate!");
-            } else{
-                System.out.println("Stalemate!");
-            }
-        }
-    }
-
     static void init() {
         MoveGeneration.initAttack();
         Zobrist.initKeys();
-    }
-
-    private static void speedTest(Board board, int repetitions) {
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < repetitions; i++) {
-            MoveGeneration.getPinnedPieces(board.fKing, board.occupied, board.eRook, board.eBishop, board.eQueen);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(end-start);
     }
 }
